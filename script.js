37e12f0e97868b6c26b3d6c9aede982427467326
LucyMurmu <btech10543.22@bitmesra.ac.in>
2026-06-06 01:37:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,42 @@
-//your JS code here. If required.
-const output=document.getElementById("output");
+// your JS code here. If required.
 
-function createPromise(){
-	const time=Math.random()*2+1;
-	return new Promise((resolve)=>{
-		setTimeout(()=>{
-			resolve(time);
-		},time*1000);
-	})
+const output = document.getElementById("output");
+
+function createPromise() {
+    const time = Math.random() * 2 + 1;
+
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(time);
+        }, time * 1000);
+    });
 }
-const start=performance.now();
+
+const start = performance.now();
 
 Promise.all([
-	createPromise(),
-	createPromise(),
-	createPromise()
-]).then((result)=>{
-	output.innerHTML="";
-	result.forEach((time,index)=>{
-		output.innerHTML+=`
-		<tr>
-		<td>Promise ${index+1}</td>
-		<td>${time.toFixed(3)}</td>
-		</tr>`;
-	});
+    createPromise(),
+    createPromise(),
+    createPromise()
+]).then((result) => {
 
-	const total=(performance.now()-start)/1000;
-	output.innerHTML+=`
-	<tr>
-	<td>Total</td>
-	<td>${total.toFixed(3)}</td>
-	</tr>`;
+    output.innerHTML = "";
+
+    result.forEach((time, index) => {
+        output.innerHTML += `
+            <tr>
+                <td>Promise ${index + 1}</td>
+                <td>${time.toFixed(3)}</td>
+            </tr>
+        `;
+    });
+
+    const total = (performance.now() - start) / 1000;
+
+    output.innerHTML += `
+        <tr>
+            <td>Total</td>
+            <td>${total.toFixed(3)}</td>
+        </tr>
+    `;
 });
